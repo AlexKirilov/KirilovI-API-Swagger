bd964b3ce8161515c55297369c5fe84999ba1cb6
Aleksandar Kirilov <Aleksandar.Kirilov@thought-train.co>
2021-12-01 18:39:43 +0000

    UI - Adding basic redux template + Store functionality

---
+++ ui/src/Core/redux/Reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+// import { appSettingsReducer } from "./appSettings.reducer";
+// import { searchFiltersReducer } from "./searchFilter.reducer";
+
+const rootReducer = combineReducers({
+    // appSettingsReducer,
+    // searchFiltersReducer
+}); 
+
+export default rootReducer;
---
+++ ui/src/Core/redux/configureStore.js
@@ -0,0 +1,37 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./Reducers/index";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+export const storeKey = 'platform_key';
+
+// convert object to string and store in localStorage
+function saveToLocalStorage(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem(storeKey, serializedState);
+    } catch (e) {
+        console.warn(e);
+    }
+}
+
+// load string from localStorage and convert into an Object
+// invalid output must be undefined
+export function loadFromLocalStorage() {
+    try {
+        const serializedState = localStorage.getItem(storeKey);
+        if (serializedState === null) return undefined;
+        return JSON.parse(serializedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+}
+
+// export type AppState = ReturnType<typeof rootReducer>;
+export const store = createStore(rootReducer, loadFromLocalStorage(), composeWithDevTools(
+    applyMiddleware(),
+));
+
+// listen for store changes and use saveToLocalStorage to
+// save them to localStorage
+store.subscribe(() => saveToLocalStorage(store.getState()));
